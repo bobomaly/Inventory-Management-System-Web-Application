d8460e90287a3658c7cf02c257acc4528e189c86
bobomaly <111780043+bobomaly@users.noreply.github.com>
2024-01-03 19:28:01 +0200

    v 0.7.03

--- user/views.py
+++ user/views.py
@@ -16,3 +16,8 @@ def register(request):
         'form': form,
     }
     return render(request, 'user/register.html', context)
+
+
+def profile(request):
+    return render(request, 'user/profile.html')
+
